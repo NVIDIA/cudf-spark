dadac77f5e31d36d307816b1e142dedcd69262e7
Firestarman <firestarmanllc@gmail.com>
2026-08-14 10:36:54 +0800

    Restore Delta deletion vector helper docs
    Signed-off-by: Firestarman <firestarmanllc@gmail.com>

--- integration_tests/src/main/python/spark_session.py
+++ integration_tests/src/main/python/spark_session.py
@@ -338,6 +338,7 @@ def is_databricks173_or_later():
     return is_databricks_version_or_later(17, 3)
 
 def supports_delta_lake_deletion_vectors():
+    """Whether the current Delta Lake runtime provides the deletion-vector feature."""
     if is_databricks_runtime():
         return is_databricks122_or_later()
     else:
