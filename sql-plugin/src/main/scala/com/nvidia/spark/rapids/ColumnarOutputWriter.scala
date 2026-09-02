0749e9378978ac94c85cbe22b9ecb680f37a9cca
Ray Liu <liurenjie2008@gmail.com>
2026-09-02 17:18:57 +0800

    Use cuDF writer footer for Iceberg metrics
    Signed-off-by: Ray Liu <liurenjie2008@gmail.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/ColumnarOutputWriter.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/ColumnarOutputWriter.scala
@@ -281,18 +281,34 @@ abstract class ColumnarOutputWriter(context: TaskAttemptContext,
    * Closes the [[ColumnarOutputWriter]]. Invoked on the executor side after all columnar batches
    * are persisted, before the task output is committed.
    */
-  def close(): Unit = {
+  private def prepareToClose(): Unit = {
     if (!anythingWritten) {
       // This prevents writing out bad files
       bufferBatchAndClose(GpuColumnVector.emptyBatch(dataSchema))
     }
-    tableWriter.close()
+  }
+
+  private def finishClose(): Unit = {
     GpuSemaphore.releaseIfNecessary(TaskContext.get())
     writeBufferedData()
     outputStream.close()
     debugDumpOutputStream.foreach { os =>
       os.close()
     }
+  }
+
+  protected final def closeAndReturn[T <: AutoCloseable](closeWriter: => T): T = {
+    prepareToClose()
+    closeOnExcept(closeWriter) { result =>
+      finishClose()
+      result
+    }
+  }
+
+  def close(): Unit = {
+    prepareToClose()
+    tableWriter.close()
+    finishClose()
   }
 
   /**
